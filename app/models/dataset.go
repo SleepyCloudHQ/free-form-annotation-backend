3a52661f9d4e4974098b4f45ddedca305c3c9c83
Igor Pidik <igorPidik@gmail.com>
2022-07-19 08:11:11 +0200

    add dataset metadata

--- app/models/dataset.go
+++ app/models/dataset.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
@@ -13,7 +14,8 @@ const (
 
 type Dataset struct {
 	gorm.Model
-	Name    string      `gorm:"not null;" json:"name"`
-	Samples []Sample    `json:"samples"`
-	Type    DatasetType `gorm:"not null" json:"type"`
+	Name     string         `gorm:"not null;" json:"name"`
+	Samples  []Sample       `json:"samples"`
+	Type     DatasetType    `gorm:"not null" json:"type"`
+	Metadata datatypes.JSON `json:"metadata"`
 }
