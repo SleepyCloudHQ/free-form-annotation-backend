fdff16294d086c49b44a6c520f5e0fd2093e1d6e
Igor Pidik <igorPidik@gmail.com>
2022-07-22 11:55:57 +0200

    user can specify default tags when importing data

--- commands/import_dataset/import_dataset.go
+++ commands/import_dataset/import_dataset.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"gopkg.in/guregu/null.v4"
 	"gorm.io/datatypes"
@@ -40,38 +41,87 @@ type SampleData struct {
 	Status      null.String    `json:"status"`
 }
 
-func mapSampleDataToSample(sampleData *SampleData, datasetId uint) *models.Sample {
-	annotationsData, err := json.Marshal(sampleData.Annotations)
-	if err != nil {
-		log.Fatal(err)
+func mapSampleDataToSample(sampleData []SampleData, datasetId uint) []models.Sample {
+	samples := make([]models.Sample, len(sampleData))
+
+	for i, d := range sampleData {
+		annotationsData, err := json.Marshal(d.Annotations)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		samples[i] = models.Sample{
+			DatasetID:   datasetId,
+			Annotations: datatypes.JSON(annotationsData),
+			Status:      d.Status,
+			Text:        d.Text,
+		}
 	}
 
-	return &models.Sample{
-		DatasetID:   datasetId,
-		Annotations: datatypes.JSON(annotationsData),
-		Status:      sampleData.Status,
-		Text:        sampleData.Text,
+	return samples
+}
+
+func loadSampleData(filePath string) ([]SampleData, error) {
+	samplesFile, fileErr := os.Open(filePath)
+	if fileErr != nil {
+		return nil, fileErr
 	}
+
+	var samplesData []SampleData
+	parsingErr := json.NewDecoder(samplesFile).Decode(&samplesData)
+	if parsingErr != nil {
+		return nil, parsingErr
+	}
+
+	return samplesData, nil
+}
+
+func createDatasetMetadata(entityTags []string, relationshipTags []string) (datatypes.JSON, error) {
+	metadata := struct {
+		EntityTags       []string `json:"entityTags"`
+		RelationshipTags []string `json:"relationshipTags"`
+	}{
+		EntityTags:       entityTags,
+		RelationshipTags: relationshipTags,
+	}
+	metadataJson, marshalErr := json.Marshal(metadata)
+	if marshalErr != nil {
+		return nil, marshalErr
+	}
+	return datatypes.JSON([]byte(metadataJson)), nil
+}
+
+func parseTags(inputData string) []string {
+	if inputData == "" {
+		return nil
+	}
+
+	return strings.Split(inputData, ",")
 }
 
 func main() {
 	datasetName := flag.String("n", "", "dataset's name")
 	samplesFilePath := flag.String("f", "", "samples file path")
+
+	predefinedEntities := flag.String("e", "", "entities")
+	predefinedRelationships := flag.String("r", "", "relationships")
+
 	flag.Parse()
 
 	if *datasetName == "" {
 		log.Fatal("Dataset name cannot be empty")
 	}
 
-	samplesFile, fileErr := os.Open(*samplesFilePath)
-	if fileErr != nil {
-		log.Fatal(fileErr)
+	entityTags := parseTags(*predefinedEntities)
+	relationshipTags := parseTags(*predefinedRelationships)
+	metadata, metadataErr := createDatasetMetadata(entityTags, relationshipTags)
+	if metadataErr != nil {
+		log.Fatal(metadataErr)
 	}
 
-	var samplesData []SampleData
-	parsingErr := json.NewDecoder(samplesFile).Decode(&samplesData)
-	if parsingErr != nil {
-		log.Fatal(parsingErr)
+	samplesData, samplesDataErr := loadSampleData(*samplesFilePath)
+	if samplesDataErr != nil {
+		log.Fatal(samplesDataErr)
 	}
 
 	db, dbErr := gorm.Open(sqlite.Open("../../test.db"))
@@ -81,18 +131,15 @@ func main() {
 
 	// create dataset
 	dataset := &models.Dataset{
-		Name: *datasetName,
+		Name:     *datasetName,
+		Metadata: metadata,
 	}
 
 	if datasetCreateErr := db.Create(&dataset).Error; datasetCreateErr != nil {
 		log.Fatal(datasetCreateErr)
 	}
 
-	samples := make([]*models.Sample, len(samplesData))
-	for i, d := range samplesData {
-		samples[i] = mapSampleDataToSample(&d, dataset.ID)
-	}
-
+	samples := mapSampleDataToSample(samplesData, dataset.ID)
 	// create samples in a batch
 	if sampleCreateErr := db.Create(&samples).Error; sampleCreateErr != nil {
 		log.Fatal(sampleCreateErr)
