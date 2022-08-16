2b9029e35a0d6f678d4a499d9b26b2ae40dc6156
Igor Pidik <igorPidik@gmail.com>
2022-08-16 07:31:17 +0200

    extract dataset import helpers

--- commands/import_dataset/import_dataset.go
+++ commands/import_dataset/import_dataset.go
@@ -3,102 +3,21 @@ package main
 import (
 	"backend/app/models"
 	"backend/app/utils"
-	"encoding/json"
+	dataset_utils "backend/app/utils/dataset"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
-	"gopkg.in/guregu/null.v4"
-	"gorm.io/datatypes"
 )
 
-type Entity struct {
-	Id    uint        `json:"id"`
-	Start uint        `json:"start"`
-	End   uint        `json:"end"`
-	Tag   null.String `json:"tag"`
-	Notes null.String `json:"notes"`
-	Color null.String `json:"color"`
-}
-
-type Relationship struct {
-	Id      uint        `json:"id"`
-	Entity1 uint        `json:"entity1"`
-	Entity2 uint        `json:"entity2"`
-	Name    string      `json:"name"`
-	Color   null.String `json:"color"`
-}
-
-type AnnotationData struct {
-	Entities      []Entity       `json:"entities"`
-	Relationships []Relationship `json:"relationships"`
-}
-
-type SampleData struct {
-	Text        string         `json:"text"`
-	Annotations AnnotationData `json:"annotations"`
-	Status      null.String    `json:"status"`
-}
-
-func mapSampleDataToSample(sampleData []SampleData, datasetId uint) []models.Sample {
-	samples := make([]models.Sample, len(sampleData))
-
-	for i, d := range sampleData {
-		annotationsData, err := json.Marshal(d.Annotations)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		samples[i] = models.Sample{
-			DatasetID:   datasetId,
-			Annotations: datatypes.JSON(annotationsData),
-			Status:      d.Status,
-			Text:        d.Text,
-		}
-	}
-
-	return samples
-}
-
-func loadSampleData(filePath string) ([]SampleData, error) {
+func loadSampleDataFromFile(filePath string) ([]dataset_utils.SampleData, error) {
 	samplesFile, fileErr := os.Open(filePath)
 	if fileErr != nil {
 		return nil, fileErr
 	}
-
-	var samplesData []SampleData
-	parsingErr := json.NewDecoder(samplesFile).Decode(&samplesData)
-	if parsingErr != nil {
-		return nil, parsingErr
-	}
-
-	return samplesData, nil
-}
-
-func createDatasetMetadata(entityTags []string, relationshipTags []string) (datatypes.JSON, error) {
-	metadata := struct {
-		EntityTags       []string `json:"entityTags"`
-		RelationshipTags []string `json:"relationshipTags"`
-	}{
-		EntityTags:       entityTags,
-		RelationshipTags: relationshipTags,
-	}
-	metadataJson, marshalErr := json.Marshal(metadata)
-	if marshalErr != nil {
-		return nil, marshalErr
-	}
-	return datatypes.JSON([]byte(metadataJson)), nil
-}
-
-func parseTags(inputData string) []string {
-	if inputData == "" {
-		return nil
-	}
-
-	return strings.Split(inputData, ",")
+	return dataset_utils.LoadSampleData(samplesFile)
 }
 
 func main() {
@@ -115,14 +34,14 @@ func main() {
 		log.Fatal("Dataset name cannot be empty")
 	}
 
-	entityTags := parseTags(*predefinedEntities)
-	relationshipTags := parseTags(*predefinedRelationships)
-	metadata, metadataErr := createDatasetMetadata(entityTags, relationshipTags)
+	entityTags := dataset_utils.ParseTags(*predefinedEntities)
+	relationshipTags := dataset_utils.ParseTags(*predefinedRelationships)
+	metadata, metadataErr := dataset_utils.CreateDatasetMetadata(entityTags, relationshipTags)
 	if metadataErr != nil {
 		log.Fatal(metadataErr)
 	}
 
-	samplesData, samplesDataErr := loadSampleData(*samplesFilePath)
+	samplesData, samplesDataErr := loadSampleDataFromFile(*samplesFilePath)
 	if samplesDataErr != nil {
 		log.Fatal(samplesDataErr)
 	}
@@ -146,7 +65,7 @@ func main() {
 		log.Fatal(datasetCreateErr)
 	}
 
-	samples := mapSampleDataToSample(samplesData, dataset.ID)
+	samples := dataset_utils.MapSampleDataToSample(samplesData, dataset.ID)
 	// create samples in a batch
 	if sampleCreateErr := db.Create(&samples).Error; sampleCreateErr != nil {
 		log.Fatal(sampleCreateErr)
