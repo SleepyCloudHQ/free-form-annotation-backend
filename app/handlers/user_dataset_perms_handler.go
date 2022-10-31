a13e1a8088a8a9e4ae2e6f699b2c354ba58e7ece
Igor Pidik <igorPidik@gmail.com>
2022-10-31 19:10:13 +0100

    handler check user perms

--- app/handlers/user_dataset_perms_handler.go
+++ app/handlers/user_dataset_perms_handler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"backend/app/models"
+	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -10,21 +12,38 @@ type DatasetToUserPermsRequest struct {
 }
 
 type UserDatasetPermsHandler struct {
-	DB *gorm.DB
+	db *gorm.DB
 }
 
 func NewUserDatasetPermsHandler(db *gorm.DB) *UserDatasetPermsHandler {
 	return &UserDatasetPermsHandler{
-		DB: db,
+		db: db,
 	}
 }
 
 func (u *UserDatasetPermsHandler) AddDatasetToUserPerms(userId uint, datasetId uint) error {
 	userDatasetPerm := &models.UserDataset{UserID: userId, DatasetID: datasetId}
-	return u.DB.Create(userDatasetPerm).Error
+	return u.db.Create(userDatasetPerm).Error
 }
 
 func (u *UserDatasetPermsHandler) DeleteDatasetToUserPerms(userId uint, datasetId uint) error {
 	userDatasetPerm := &models.UserDataset{UserID: userId, DatasetID: datasetId}
-	return u.DB.Delete(userDatasetPerm).Error
+	return u.db.Delete(userDatasetPerm).Error
+}
+
+func (u *UserDatasetPermsHandler) UserHasDatasetPerms(userId uint, datasetId uint) (bool, error) {
+	// check if the user has been assigned to the given dataset
+	userDataset := &models.UserDataset{
+		UserID:    userId,
+		DatasetID: datasetId,
+	}
+
+	if result := u.db.First(userDataset); result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+		return false, result.Error
+	}
+
+	return true, nil
 }
