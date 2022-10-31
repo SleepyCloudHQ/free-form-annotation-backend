8160005e7ac67fd433b37c57df661f9a7a24cb75
Igor Pidik <igorPidik@gmail.com>
2022-10-31 19:09:52 +0100

    clean up is admin middleware

--- app/middlewares/is_admin_middleware_test.go
+++ app/middlewares/is_admin_middleware_test.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"backend/app/auth"
 	"backend/app/models"
 	"context"
 	"net/http"
@@ -9,40 +8,11 @@ import (
 	"testing"
 
 	"github.com/matryer/is"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
-func setupDBForIsAdminMiddlewareTests(t *testing.T) (*gorm.DB, func() error) {
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
-
-	if err != nil {
-		t.Fatalf("failed to open sqlite connection: %v", err)
-	}
-
-	if migrationErr := db.AutoMigrate(&models.User{}); migrationErr != nil {
-		t.Fatalf("failed to migrate user: %v", migrationErr)
-	}
-
-	sqlDB, sqlErr := db.DB()
-	if sqlErr != nil {
-		t.Fatalf("failed to obtain SQL DB: %v", sqlErr)
-	}
-	return db, sqlDB.Close
-}
-
 func TestIsAdminMiddlewareWithAdminRole(t *testing.T) {
-	db, cleanup := setupDBForIsAdminMiddlewareTests(t)
-	defer cleanup()
-
 	is := is.New(t)
-	userAuth := auth.NewUserAuth(db)
-
-	user, userErr := userAuth.CreateUser("user@email.com", "pass", models.AdminRole)
-	is.NoErr(userErr)
+	user := &models.User{Role: models.AdminRole}
 
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -58,14 +28,8 @@ func TestIsAdminMiddlewareWithAdminRole(t *testing.T) {
 }
 
 func TestIsAdminMiddlewareWithAnnotatorRole(t *testing.T) {
-	db, cleanup := setupDBForIsAdminMiddlewareTests(t)
-	defer cleanup()
-
 	is := is.New(t)
-	userAuth := auth.NewUserAuth(db)
-
-	user, userErr := userAuth.CreateUser("user@email.com", "pass", models.AnnotatorRole)
-	is.NoErr(userErr)
+	user := &models.User{Role: models.AnnotatorRole}
 
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t.Fatal("request should be stopped by the middleware anod it should not reach here")
